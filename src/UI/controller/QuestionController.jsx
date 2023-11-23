e8d7c7ade210067d3ca430b035c456e1908a779b
Paula Oseroff <oseroffp@gmail.com>
2023-11-22 23:01:28 -0300

    feat: allow one correct and one incorrect answer

--- src/UI/controller/QuestionController.jsx
+++ src/UI/controller/QuestionController.jsx
@@ -39,11 +39,11 @@ const QuestionController = ({showLogin, setShowLogin, authActionsPending, setAut
 
     const addItem  = async (question, setAlert) => {
         if (!question.name || !question.answers || !question.correctAnswer){
-            setAlert("Necesitás especificar nombre, respuesta correcta y, al menos, 2 otras opciones.");
+            setAlert("Necesitás especificar nombre, respuesta correcta y, al menos, 2 opciones.");
             return false;
         }
-        if(question.answers.length < 2) {
-            setAlert("Necesitás especificar al menos 2 otras opciones.");
+        if(question.answers.length < 1) {
+            setAlert("Necesitás especificar al menos una opción incorrecta.");
             return false;
         }
         setMenuAlert({variant: "primary", value:"Espere..."});
@@ -80,11 +80,11 @@ const QuestionController = ({showLogin, setShowLogin, authActionsPending, setAut
             return false;
         }
         if (!question.name || !question.answers || !question.correctAnswer){
-            setAlert("Necesitás especificar nombre, respuesta correcta y, al menos, 2 otras opciones.");
+            setAlert("Necesitás especificar nombre, respuesta correcta y, al menos, 2 opciones.");
             return false;
         }
-        if(question.answers.length < 2) {
-            setAlert("Necesitás especificar al menos 2 otras opciones.");
+        if(question.answers.length < 1) {
+            setAlert("Necesitás especificar al menos una opción incorrecta.");
             return false;
         }
         setMenuAlert({variant: "primary", value:"Espere..."});
