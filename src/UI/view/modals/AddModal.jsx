3c600af6571c0922a79ce0e2d6261b68b3372800
Paula Oseroff <oseroffp@gmail.com>
2024-01-16 23:42:18 -0300

    feat: add styling for selecting set type when adding set

--- src/UI/view/modals/AddModal.jsx
+++ src/UI/view/modals/AddModal.jsx
@@ -1,11 +1,18 @@
+import { faFolderOpen, faList } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
-import { Alert, Button, Form, Modal } from "react-bootstrap";
+import { Alert, Button, ButtonGroup, Form, Modal, ToggleButton } from "react-bootstrap";
 
 
 const AddModal = ({title, addItem, confirmButton, icon, show, setShow}) => {
+    const setTypes = [
+        { name : "Preguntas", value : "questions", icon : faList },
+        { name : "Carpeta", value : "folder", icon : faFolderOpen }
+    ];
+
     const [name, setName] = useState("");
     const [alert, setAlert] = useState("");
+    const [selectedType, setSelectedType] = useState(setTypes[0].value);
 
     const handleClose = () => {
         setShow(false);
@@ -45,6 +52,34 @@ const AddModal = ({title, addItem, confirmButton, icon, show, setShow}) => {
                         onChange={(e) => setName(e.target.value)}
                         autoFocus
                     />
+                    {
+                        false ? (
+                            <Form.Group
+                                className="mt-3 d-flex align-items-center"
+                            >
+                                <ButtonGroup
+                                    className="flex-grow-1 p-3 border d-flex"
+                                >
+                                    {setTypes.map((setType, idx) => (
+                                        <ToggleButton
+                                            key={idx}
+                                            id={`settype-${idx}`}
+                                            type="radio"
+                                            variant={selectedType === setType.value ? "outline-primary" : "outline-dark"}
+                                            name="radio"
+                                            value={setType.value}
+                                            checked={selectedType === setType.value}
+                                            onChange={(e) => setSelectedType(e.currentTarget.value)}
+                                            className="no-flex-basis d-flex flex-column p-3"
+                                        >
+                                            <FontAwesomeIcon icon={setType.icon} className="text-2 mb-2"/>
+                                            <small>{setType.name}</small>
+                                        </ToggleButton>
+                                    ))}
+                                </ButtonGroup>
+                            </Form.Group>
+                        ) : ""
+                    }
                 </Modal.Body>
                 <Modal.Footer className="align-items-stretch">
                     <Button variant="secondary" onClick={handleClose} className="flex-fill no-flex-basis p-3">
